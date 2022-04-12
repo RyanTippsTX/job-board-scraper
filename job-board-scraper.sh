bbdfd5ad0102b9ff7a860a55bca0cffc764c2fcf
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2022-04-12 13:02:40 -0500

    WIP - only works with no spaces in location (e.g. "Austin,TX" but not "San Francisco, CA")

--- job-board-scraper.sh
+++ job-board-scraper.sh
@@ -1,37 +1,91 @@
 #!/bin/zsh
 
 # Author : Ryan Tipps
-# Copyright (c) 2022 Ryan Tipps
+# License : MIT
 
-# source URL example:
-# https://www.indeed.com/jobs?q=rails&l=remote&fromage=14
+scrape_Indeed () { 
+  # Scrapes the reported number of matching job postings from an indivudual query on Indeed.com job board.
 
-# returns number of postings for one location & one term
-poll-Indeed-single () { 
-  LOCATION=$(echo $1)
-  KEYWORD=$(echo $2)
-  curl -s "https://www.indeed.com/jobs?q=$KEYWORD&l=$LOCATION&fromage=14" | grep -E "[0-9]+ jobs</div>" | cut -d' ' -f 24
-  # curl -s "https://www.indeed.com/jobs?q=React&l=Remote&fromage=14" | grep -E "[0-9]+ jobs</div>" | cut -d' ' -f 24
+  # Build URL based on search parameters:
+  # NOTE: Indeed URL-encodes spaces (%20) but NOT commas. This is non-standard. Therefore we have to manually URL-encode rather than using curl's built-in URL encoding.
+  location=$1
+  search_term=$2
+
+  # testing='hello world'
+  # echo ${testing//' '/'%20'}
+
+  scrape_url="https://www.indeed.com/jobs"
+  scrape_url+="?"
+  scrape_url+="q=${search_term//' '/'%20'}"
+  scrape_url+="&"
+  scrape_url+="q=${location//' '/'%20'}"
+  # if [ ${location,,} != 'remote' ] 
+  # then
+  #   scrape_url+="&"
+  #   scrape_url+="radius=50"
+  # fi
+  scrape_url+="&"
+  scrape_url+="fromage=14"
+  # Typical resulting URL: https://www.indeed.com/jobs?q=Java&l=remote&fromage=14
+  
+
+  # Fetch page from Indeed and scrape the reported result:
+  # This is very site specific and prone to breaking. Needs improvement!
+  curl -s $scrape_url | grep -E "[0-9]+ jobs</div>" | cut -d' ' -f 24
 }
 
-# GENERATE REPORT
-echo -e 'Indeed.com job postings in last 14 days by location and keyword mentions:\n'
-echo -e "KEYWORD \tREMOTE \t\tAUSTIN"  
-# locations not are wired in manually for now... 
-# declare -a LOCATIONS=(
-#   'Remote' 
-#   'Austin,TX&radius=50'
-#   )
-declare -a SEARCH_TERMS=(
-  'Rails' 
-  'Django' 
-  'Node'
-  'Angular'
-  'React'
-  'Vue'
-  )
-for t in "${SEARCH_TERMS[@]}"
-do
-   echo -e "$t\t\t$( poll-Indeed-single 'Remote' $t )\t\t$( poll-Indeed-single 'Austin,TX&radius=50' $t )" &
-done
-wait
+
+run_report (){
+  # Aggregates data and prints report to stdout
+
+  echo -e 'Indeed.com job postings in last 14 days by location and keyword mentions:\n'
+  echo -e "SEARCH TERM \tREMOTE \t\tAUSTIN"  
+
+  # locations not are wired in manually for now... 
+  # declare -a LOCATIONS=(
+  #   'Remote' 
+  #   'Austin,TX&radius=50'
+  #   )
+
+  declare -a search_terms=(
+    'Rails' 
+    'Django' 
+    'Node'
+    'Angular'
+    'React'
+    'Vue'
+    )
+  for t in "${search_terms[@]}"
+  do
+    echo -e "$t\t\t$( scrape_Indeed 'Remote' $t )\t\t$( scrape_Indeed 'Austin,TX&radius=50' $t )" &
+  done
+  wait
+}
+
+# Execute !!
+run_report
+
+
+# location="San Francisco, CA"
+# search_term="Python"
+
+#   # testing='hello world'
+#   # echo ${testing//' '/'%20'}
+
+#   scrape_url="https://www.indeed.com/jobs"
+#   scrape_url+="?"
+#   scrape_url+="q=${search_term//' '/'%20'}"
+#   scrape_url+="&"
+#   scrape_url+="q=${location//' '/%20}"
+#   if [ ${location} != 'remote' ] 
+#   then
+#     scrape_url+="&"
+#     scrape_url+="radius=50"
+#   fi
+#   scrape_url+="&"
+#   scrape_url+="fromage=14"
+#   echo $scrape_url
+
+# curl $scrape_url
+# curl 'https://www.indeed.com/jobs?q=Python&q=San%20Francisco,%20CA&radius=50&fromage=14'
+# curl 'https://www.indeed.com/jobs?q=python&l=San Francisco%2C CA'
